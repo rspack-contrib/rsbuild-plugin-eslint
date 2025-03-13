d0751b48b0b73db1cbc0253a4ec06d9ff77aba65
neverland <chenjiahan.jait@bytedance.com>
2025-03-13 21:25:54 +0800

    test: update error message

--- test/flat-config/index.test.ts
+++ test/flat-config/index.test.ts
@@ -23,7 +23,7 @@ test('should throw error when exist ESLint errors', async () => {
 			],
 		},
 	});
-	await expect(rsbuild.build()).rejects.toThrowError('build failed!');
+	await expect(rsbuild.build()).rejects.toThrowError('build failed');
 
 	expect(
 		logs.find((log) => log.includes(`'undefinedVar' is not defined`)),
